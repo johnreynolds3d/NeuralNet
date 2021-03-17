69ea4417b08c529aef6c713991de219361eb4218
John Reynolds <felixinutero@gmail.com>
2021-03-17 17:07:18 +0800

    added MacOS build

--- src/main.c
+++ src/main.c
@@ -7,9 +7,9 @@ int main() {
 
   srand(time(NULL));
 
-  int num_layers = 1;
-  int num_neurons = 2;
-  int num_neuron_inputs = 4;
+  int num_layers = 2;
+  int num_neurons = 4;
+  int num_neuron_inputs = 8;
 
   struct Layer *layers[num_layers];
 
@@ -21,17 +21,17 @@ int main() {
 
     for (nn = 0; nn < num_neurons; nn++) {
 
-      printf("\nlayers[%d]->neurons[%d]->bias: %f\n\n", l + 1, nn + 1,
+      printf("\nlayers[%d]->neurons[%d]->bias:\t\t%9f\n\n", l + 1, nn + 1,
              layers[l]->neurons[nn]->bias);
 
       for (nni = 0; nni < num_neuron_inputs; nni++) {
-        printf("layers[%d]->neurons[%d]->weights[%d]: %f\n", l + 1, nn + 1,
+        printf("layers[%d]->neurons[%d]->weights[%d]:\t%9f\n", l + 1, nn + 1,
                nni + 1, layers[l]->neurons[nn]->weights[nni]);
       }
       printf("\n");
 
       for (nni = 0; nni < num_neuron_inputs; nni++) {
-        printf("layers[%d]->neurons[%d]->inputs[%d]: %f\n", l + 1, nn + 1,
+        printf("layers[%d]->neurons[%d]->inputs[%d]:\t%9f\n", l + 1, nn + 1,
                nni + 1, layers[l]->neurons[nn]->inputs[nni]);
       }
       printf("\n");
