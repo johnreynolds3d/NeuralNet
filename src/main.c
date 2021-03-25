909fa494bb211f0ce1c469c4db8bad47c481ff5b
John Reynolds <felixinutero@gmail.com>
2021-03-25 20:41:23 +0800

    cleaning up

--- src/main.c
+++ src/main.c
@@ -23,18 +23,18 @@ int main() {
 
     for (nn = 0; nn < num_neurons; nn++) {
 
-      printf("\nlayers[%d]->neurons[%d]->bias:\t\t%9f\n\n", l + 1, nn + 1,
+      printf("\n\tlayers[%d]->neurons[%d]->bias:\t\t%9f\n\n", l, nn,
              layers[l]->neurons[nn]->bias);
 
       for (nni = 0; nni < num_neuron_inputs; nni++) {
-        printf("layers[%d]->neurons[%d]->weights[%d]:\t%9f\n", l + 1, nn + 1,
-               nni + 1, layers[l]->neurons[nn]->weights[nni]);
+        printf("\tlayers[%d]->neurons[%d]->weights[%d]:\t%9f\n", l, nn, nni + 1,
+               layers[l]->neurons[nn]->weights[nni]);
       }
       printf("\n");
 
       for (nni = 0; nni < num_neuron_inputs; nni++) {
-        printf("layers[%d]->neurons[%d]->inputs[%d]:\t%9f\n", l + 1, nn + 1,
-               nni + 1, layers[l]->neurons[nn]->inputs[nni]);
+        printf("\tlayers[%d]->neurons[%d]->inputs[%d]:\t%9f\n", l, nn, nni + 1,
+               layers[l]->neurons[nn]->inputs[nni]);
       }
       printf("\n");
     }
