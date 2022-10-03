1cc428bff28b2b766ab626ef4035b46d26987b91
John Reynolds <john@johnreynolds3d.com>
2022-10-03 16:00:52 +0800

    Minor formatting tweaks.

--- src/main.c
+++ src/main.c
@@ -4,14 +4,16 @@
 #include "../lib/neuralnet.h"
 
 /*
- * Gaze in disbelief as an Artificial Neural Network (AKA Multilayer Perceptron) does its darndest
- * to learn, and perform. the logical operations AND, NAND, OR, NOR, XOR, and XNOR!
+ * Gaze in disbelief as an Artificial Neural Network (AKA Multilayer Perceptron) 
+ * does its darndest to learn, and perform. the logical operations AND, NAND, OR, 
+ * NOR, XOR, and XNOR!
  *
  * Author:  John Reynolds
- * Version: 16.01.22
+ * Version: 3 October 2022
  */
 
 int main() {
+
   for (uint_fast8_t i = 0; i < 6; i++) {
     PreTraining(i);
   }
